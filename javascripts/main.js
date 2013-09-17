e23feb9dcfd7a464f98cac91a7c8d4977c1c3058
Zoltan <zdebre@gmail.com>
2013-09-17 01:09:37 +0100

    form and responsive css fixed

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,56 @@
-var sendEmail = function(event) {
+var sendEmail = function(event)
+
+{
+
+
+
+    if (request) {
+        request.abort();
+    }
+
+    var $form = $(this).closest('form');
+
+    var $inputs= $form.find('input, select, button, textarea');
+
+//    var serializedData = $form.serialize();
+
+    $inputs.prop("disabled", true);
+
+    var fullmessage = {
+            name: $('#name').val(),
+            email: $('#email').val(),
+            message: $('#message').val()
+    }
+
+    var request = $.ajax({
+        url: 'http://100pcarabica.com/send.php',
+        type: 'post',
+        data: fullmessage,
+        dataType: 'json'
+    });
+
+    request.done(function (response, textStatus, jqXHR) {
+        console.log("Response: " + response);
+        console.log("textStatus: " + textStatus);
+        console.log("jqXHR: " + jqXHR);
+
+    });
+
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        console.error(
+            "The following error occured: " +
+                textStatus, errorThrown
+        );
+    });
+
+    request.always(function() {
+        $inputs.prop('disabled', false);
+    })
+
     event.preventDefault();
-    $.get('send.php');
 
 }
+
 
 $(document).ready(function(){
     $('#send_button').on('click', sendEmail);
